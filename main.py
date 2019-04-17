3da51363a5857b80b28dc7a1833529d4fc237dc3
Shuvendu Bikash <bikash.shuvendu@gmail.com>
2019-04-17 10:17:03 +0600

    argparse added

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from queue import Queue
 from spider import Spider
 from domain import *
 from general import *
+import argparse
 
-PROJECT_NAME = 'prothom_alo'
-HOMEPAGE = 'https://www.prothomalo.com/archive/'
+parser = argparse.ArgumentParser()
+parser.add_argument("--project_name", type=str, required=True)
+parser.add_argument("--home_page", type=str, required=True)
+args = parser.parse_args()
+
+PROJECT_NAME = args.project_name  # 'prothom_alo'
+HOMEPAGE = args.home_page  # 'https://www.prothomalo.com/archive/' #
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
 NUMBER_OF_THREADS = 48
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+print(PROJECT_NAME)
+print(HOMEPAGE)
 
 
 # Create worker threads (will die when main exits)
